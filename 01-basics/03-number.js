5906985aecbcf2adbdeb087d552fd3c3803887c1
himanshu-singh-52 <himanshu112002singh@gmail.com>
2025-08-29 17:55:52 +0000

    number and math

---
+++ 01-basics/03-number.js
@@ -0,0 +1,11 @@
+const number = 15.364;
+
+console.log(number.toPrecision(4));
+
+const salary = 2000000;
+console.log(salary.toLocaleString('en-In'));
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(-4.5));
+console.log(Math.round(4.5));
--- 01-basics/test1.js
+++ 01-basics/test1.js
@@ -1,8 +1,2 @@
+
 console.log("Himanshu");
-
-const emailId = 12345;
-let acountEmail = "hima@gmail.com";
-var accountPassword = "1234";
-accountCity = "jaunpur";
-
-console.log(emailId);
