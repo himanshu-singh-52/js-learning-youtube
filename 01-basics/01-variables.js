f9baf465d266596412929ba756f864e31ebdcbce
himanshu-singh-52 <himanshu112002singh@gmail.com>
2025-08-29 17:55:09 +0000

    number and math

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,13 @@
+const emailId = 12345;
+let acountEmail = "hima@gmail.com";
+var accountPassword = "1234";
+accountCity = "jaunpur";
+let accountState;
+
+console.log(emailId);
+
+acountEmail = "hari@gmail.com";
+accountPassword = "9876";
+accountCity = "Prayagraj";
+
+console.table([acountEmail, accountPassword, accountCity, accountState]);
--- 01-basics/test1.js
+++ 01-basics/test1.js
@@ -1,1 +1,8 @@
 console.log("Himanshu");
+
+const emailId = 12345;
+let acountEmail = "hima@gmail.com";
+var accountPassword = "1234";
+accountCity = "jaunpur";
+
+console.log(emailId);
